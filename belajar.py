5f7ad1b38adbd3ce634e6e47bbb4448f36b98847
rafdmrs <123362298+rafdmrs@users.noreply.github.com>
2024-03-09 20:14:03 +0700

    Add files via upload

---
+++ belajar.py
@@ -0,0 +1,17 @@
+first_name = (' Rafael')
+last_name = (' Destiano')
+full_name = first_name + last_name
+umur = 18
+pekerjaan = ('Pelajar')
+alamat = ('Jl. Pahlawan No. 1')
+status = ('Belum Menikah')
+hobi = ('Bermusik')
+
+
+print('Hello' + full_name)
+print('Umur :', umur)
+print('Pekerjaan :', pekerjaan)
+print('Alamat :', alamat)   
+print('Status :', status)
+print('Hobi :', hobi)
+
